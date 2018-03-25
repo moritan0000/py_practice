02e0a1a936539c4b9d13250ba8a4acda1b5e488b
Hirotaka Morishita <moritan0000@gmail.com>
2018-03-26 01:24:25 +0900

    [ゼロから作るDeep Learning]勉強中

--- zero_deep-learning.py
+++ zero_deep-learning.py
@@ -18,17 +18,99 @@ print(A[np.array([0, 2, 4, 6])])
 print(A > 4)
 print(A[A > 4])
 
-x = np.arange(0, np.pi * 2, 0.1)
-y1 = np.sin(x)
-y2 = np.cos(x)
-y3 = np.sin(x ** 2)
-y4 = np.cos(2 ** x)
-plt.plot(x, y1, label="sinx")
-plt.plot(x, y2, linestyle="--", label="cosx")
-plt.plot(x, y3, label="sin(x^2)")
-plt.plot(x, y4, label="cos(2^x)")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("sin, cos")
-plt.legend()
-plt.show()
+
+def plot_sincos():
+    x = np.arange(0, np.pi * 2, 0.1)
+    y1 = np.sin(x)
+    y2 = np.cos(x)
+    y3 = np.sin(x ** 2)
+    y4 = np.cos(2 ** x)
+    plt.plot(x, y1, label="sinx")
+    plt.plot(x, y2, linestyle="--", label="cosx")
+    plt.plot(x, y3, label="sin(x^2)")
+    plt.plot(x, y4, label="cos(2^x)")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("sin, cos")
+    plt.legend()
+    plt.show()
+
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+print("AND :", AND(0, 0), AND(1, 0), AND(0, 1), AND(1, 1))
+print("NAND:", NAND(0, 0), NAND(1, 0), NAND(0, 1), NAND(1, 1))
+print("OR  :", OR(0, 0), OR(1, 0), OR(0, 1), OR(1, 1))
+print("XOR :", XOR(0, 0), XOR(1, 0), XOR(0, 1), XOR(1, 1))
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def plot_step():
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_function(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
+
+
+def plot_sigmoid():
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = sigmoid(x)
+    plt.plot(y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
+
+
+A = np.array([[1, 2, 3], [4, 5, 6]])
+B = np.array([[1, 2], [3, 4], [5, 6]])
+print("A dot B:\n", np.dot(A, B))
+print("B dot A:\n", np.dot(B, A))
