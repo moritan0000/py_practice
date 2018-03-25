b5f320ff4bb4b9742e758ad4f1504e7e42b3f83d
Hirotaka Morishita <moritan0000@gmail.com>
2018-03-26 01:39:42 +0900

    [ゼロから作るDeep Learning]勉強中

--- zero_deep-learning.py
+++ zero_deep-learning.py
@@ -113,4 +113,9 @@ def plot_sigmoid():
 A = np.array([[1, 2, 3], [4, 5, 6]])
 B = np.array([[1, 2], [3, 4], [5, 6]])
 print("A dot B:\n", np.dot(A, B))
-print("B dot A:\n", np.dot(B, A))
+print("B dot A:\n", np.dot(B, A),"\n")
+
+X = np.array([1, 2])
+W = np.array([[1, 3, 5], [2, 4, 6, ]])
+Y = np.dot(X, W)
+print(Y)
