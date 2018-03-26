c2c6543559baeebddc8d709d24d8e76460fb32bd
Hirotaka Morishita <moritan0000@gmail.com>
2018-03-27 02:16:00 +0900

    [ゼロから作るDeep Learning]勉強中

--- zero_deep-learning.py
+++ zero_deep-learning.py
@@ -6,7 +6,10 @@ y = np.array([2.0, 4.0, 6.0])
 print(x * y)
 print(x ** 5)
 
-A = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+A = np.array([[1, 2],
+              [3, 4],
+              [5, 6],
+              [7, 8]])
 B = np.array([10, 20])
 print(A * B)
 print(A.shape, A.dtype)
@@ -94,28 +97,84 @@ def relu(x):
     return np.maximum(0, x)
 
 
-def plot_step():
-    x = np.arange(-5.0, 5.0, 0.1)
-    y = step_function(x)
-    plt.plot(x, y)
+def plot_step_and_sigmoid():
+    x = np.arange(-10.0, 10.0, 0.1)
+    y1 = step_function(x)
+    y2 = sigmoid(x)
+    plt.plot(x, y1)
+    plt.plot(x, y2)
     plt.ylim(-0.1, 1.1)
     plt.show()
 
 
-def plot_sigmoid():
-    x = np.arange(-5.0, 5.0, 0.1)
-    y = sigmoid(x)
-    plt.plot(y)
-    plt.ylim(-0.1, 1.1)
-    plt.show()
-
-
-A = np.array([[1, 2, 3], [4, 5, 6]])
-B = np.array([[1, 2], [3, 4], [5, 6]])
+A = np.array([[1, 2, 3],
+              [4, 5, 6]])
+B = np.array([[1, 2],
+              [3, 4],
+              [5, 6]])
 print("A dot B:\n", np.dot(A, B))
-print("B dot A:\n", np.dot(B, A),"\n")
+print("B dot A:\n", np.dot(B, A), "\n")
 
-X = np.array([1, 2])
-W = np.array([[1, 3, 5], [2, 4, 6, ]])
-Y = np.dot(X, W)
-print(Y)
+
+def identity_function(x):
+    return x
+
+
+def init_network():
+    network = {}
+    network["W1"] = np.array([[0.1, 0.3, 0.5],
+                              [0.2, 0.4, 0.6]])
+    network["b1"] = np.array([0.1, 0.2, 0.3])
+    network["W2"] = np.array([[0.1, 0.4],
+                              [0.2, 0.5],
+                              [0.3, 0.6]])
+    network["b2"] = np.array([0.1, 0.2])
+    network["W3"] = np.array([[0.1, 0.3],
+                              [0.2, 0.4]])
+    network["b3"] = np.array([0.1, 0.2])
+
+    return network
+
+
+def forward(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y, "\n")
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+print(y, "SUM =", np.sum(y), "\n")
+
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
