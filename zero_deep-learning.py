053e9a582492cc7125741dd901012a700b2b37ce
Hirotaka Morishita <moritan0000@gmail.com>
2018-03-29 02:08:36 +0900

    [ゼロから作るDeep Learning]勉強中

--- zero_deep-learning.py
+++ zero_deep-learning.py
@@ -239,3 +239,96 @@ for i in range(0, len(x), batch_size):
     accuracy_cnt += np.sum(p == t[i:i + batch_size])
 
 print("Accuracy:", str(float(accuracy_cnt) / len(x)))
+
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+
+t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+y1 = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0])
+y2 = np.array([0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0])
+print(mean_squared_error(y1, t))
+print(mean_squared_error(y2, t))
+print(cross_entropy_error(y1, t))
+print(cross_entropy_error(y2, t))
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+print(x_train.shape, t_train.shape)
+
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
+
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x + h) - f(x - h)) / 2 * h
+
+
+def function_1(x):
+    return 0.02 * x ** 2 + 0.1 * x
+
+
+def function_2(x):
+    return x[0] ** 2 + x[1] ** 2
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h  # f(x+h)
+        fxh1 = f(x)
+        x[idx] = tmp_val - h  # f(x-h)
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    x_history = []
+
+    for i in range(step_num):
+        x_history.append(x.copy())
+
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x, np.array(x_history)
+
+
+init_x = np.array([-3.0, 4.0])
+lr = 0.1
+step_num = 100
+x, x_history = gradient_descent(function_2, init_x, lr=lr, step_num=step_num)
+print(x)
+
+plt.plot([-5, 5], [0, 0], '--b')
+plt.plot([0, 0], [-5, 5], '--b')
+plt.plot(x_history[:, 0], x_history[:, 1], 'o')
+plt.xlim(-3.5, 3.5)
+plt.ylim(-4.5, 4.5)
+plt.xlabel("X0")
+plt.ylabel("X1")
+plt.show()
