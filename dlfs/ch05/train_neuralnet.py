d1475638dd0527a202821c2d613ecea1c7edde76
Hirotaka Morishita <moritan0000@gmail.com>
2018-03-31 21:26:44 +0900

    [ゼロから作るDeep Learning]勉強中

--- dlfs/ch05/train_neuralnet.py
+++ dlfs/ch05/train_neuralnet.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 import sys, os
+
 sys.path.append(os.pardir)
 
 import numpy as np
+import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
@@ -14,11 +16,12 @@ network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
-learning_rate = 0.1
+learning_rate = 0.05
 
 train_loss_list = []
 train_acc_list = []
 test_acc_list = []
+i_list = []
 
 iter_per_epoch = max(train_size / batch_size, 1)
 
@@ -26,21 +29,33 @@ for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    
+
     # 勾配
-    #grad = network.numerical_gradient(x_batch, t_batch)
+    # grad = network.numerical_gradient(x_batch, t_batch)
     grad = network.gradient(x_batch, t_batch)
-    
+
     # 更新
     for key in ('W1', 'b1', 'W2', 'b2'):
         network.params[key] -= learning_rate * grad[key]
-    
+
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-    
+
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
+        i_list.append(i)
         print(train_acc, test_acc)
+
+
+plt.plot(i_list, train_acc_list, label="Train")
+plt.plot(i_list, test_acc_list, label="Test", linestyle="--")
+plt.xlabel("Iteration")
+plt.ylabel("Accuracy")
+plt.title("DL of MNIST")
+plt.legend()
+
+#plt.plot([x for x in range(iters_num)],train_loss_list)
+plt.show()
--- dlfs/common/layers.py
+++ dlfs/common/layers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
-from functions import *
-from util import im2col, col2im
+from common.functions import *
+from common.util import im2col, col2im
 
 
 class Relu:
