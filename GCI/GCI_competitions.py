d2d530fd491295282edfab149be1b0cd46806860
Hirotaka Morishita <moritan0000@gmail.com>
2018-04-23 20:25:51 +0900

    Solved GCI problems

--- GCI/GCI_competitions.py
+++ GCI/GCI_competitions.py
@@ -1,4 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("")
+gci_compe_path = "../../../../weblab/weblab_datascience/competitions/"
+
+train = pd.read_csv(gci_compe_path + "wine_quality/train.csv").values
+print(train)
