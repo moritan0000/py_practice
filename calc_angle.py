c908cbbbd3b130ce1be0207b6c26f6592fc44a8a
moritan0000 <moritan0000@gmail.com>
2019-06-05 02:24:00 +0900

    added function to analyze training data

--- calc_angle.py
+++ calc_angle.py
@@ -12,4 +12,29 @@ def angle():
     print(math.degrees(math.atan2(c * abs(z2 - z1), ((a * (x2 - x1)) ** 2 + (b * (y2 - y1)) ** 2) ** 0.5)))
 
 
-angle()
+# angle()
+
+def describe_data():
+    import pandas as pd
+
+    with open("C:/Linux_home/jazz/LAMMPS/Si_slab/MD/Si_slab_4_wo_tag.xyz") as f:
+        raw_data = f.readlines()
+
+    force = [[0] for _ in range(len(raw_data))]
+    for i in range(len(raw_data)):
+        line = raw_data[i].split()
+        tmp = []
+        tmp.append(line[0])
+        tmp.append(int(line[1]))
+        tmp.append(line[2])
+        tmp.extend(list(map(float, line[3:])))
+        force[i] = tmp
+
+    force = pd.DataFrame(force, columns=["file", "no", "atom", "x", "y", "z", "f_x", "f_y", "f_z"])
+    force["f_sum"] = force[["f_x", "f_y", "f_z"]].sum(axis=1)
+    # print(force)
+    print(force.describe())
+    force.to_csv("C:/Users/HirotakaMorishita/Desktop/force_4.csv")
+
+
+describe_data()
